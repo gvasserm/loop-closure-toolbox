0fddcc76b6d184c637f56b2ef45ef964903fc12d
Genadiy Vasserman <genadiy.vasserman@intel.com>
2024-05-20 16:01:07 +0300

    cleaned the code

--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import copy
+import numpy as np
 
 def sort_key(filename, id_split='im'):
     # Extract the base name (e.g., "1" from "1.jpg")
@@ -30,3 +32,47 @@ def load_descriptors(file_path):
     file_storage.release()
     
     return descriptors
+
+def deserializeKeyPointsFromSimpleFormat(filepath):
+    keypoints = []
+    with open(filepath, 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 7:
+                x, y, size, angle, response, octave = map(float, parts[:6])
+                class_id = int(parts[6])
+                kp = cv2.KeyPoint(x=x, y=y, size=size, angle=angle, response=response, octave=int(octave), class_id=class_id)
+                keypoints.append(kp)
+    return keypoints
+
+
+def get_descriptors_orb(img_path):
+    if type(img_path) == str:
+        image = cv2.imread(img_path)
+    else:
+        image = img_path
+    # Initialize ORB detector
+    orb = cv2.ORB_create(2000)
+    # Detect keypoints
+    keypoints = orb.detect(image, None)
+    # Compute descriptors
+    keypoints, descriptors = orb.compute(image, keypoints)
+
+    return keypoints, descriptors
+
+def draw_keypoints(img, keypoints, size=4, color=(0, 0, 255)):
+    """
+    
+    """
+    out_im = copy.deepcopy(img)
+    if len(out_im.shape) == 2:
+        out_im = cv2.cvtColor(out_im, cv2.COLOR_GRAY2RGB)
+    # Draw matches
+    for kp in keypoints:
+        # Get the matching keypoints in both images
+        pt = tuple(np.round(kp.pt).astype(int))
+        
+        # Draw the keypoints
+        cv2.circle(out_im, pt, size, color, -1, cv2.LINE_AA)
+    
+    return out_im
